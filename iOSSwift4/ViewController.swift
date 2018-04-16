f14ad0a2eedab5cda1200071e62720e94b53d84e
syntax3rror <winzter@gmail.com>
2018-04-16 11:14:14 +0800

    Addinf the initial single app swift

---
+++ iOSSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSwift4
+//
+//  Created by Xurpas Enterprise on 16/04/2018.
+//  Copyright © 2018 Xurpas Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
